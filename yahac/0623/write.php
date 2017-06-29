27276fdef48ad8555ce5484af8c21d61312163e5
Terry Yun <nooheat1228@gmail.com>
2017-06-29 22:53:53 +0900

    06/23 ~관계형 데이터베이스 실습

--- yahac/0623/write.php
+++ yahac/0623/write.php
@@ -12,13 +12,13 @@ $result = mysqli_query($conn, "SELECT * FROM topic");
 <body id="target">
     <header>
     <img src="https://s3.ap-northeast-2.amazonaws.com/opentutorials-user-file/course/94.png" alt="생활코딩">
-        <h1><a href="http://localhost:8001/yahac/0622/index.php">JavaScript</a></h1>
+        <h1><a href="http://localhost:8001/yahac/0623/index.php">JavaScript</a></h1>
   </header>
     <nav>
         <ol>
     <?php
     while( $row = mysqli_fetch_assoc($result)){
-      echo '<li><a href="http://localhost:8001/yahac/0622/index.php?id='.$row['id'].'">'.$row['title'].'</a></li>'."\n";
+      echo '<li><a href="http://localhost:8001/yahac/0623/index.php?id='.$row['id'].'">'.$row['title'].'</a></li>'."\n";
     }
     ?>
         </ol>
@@ -26,7 +26,7 @@ $result = mysqli_query($conn, "SELECT * FROM topic");
   <div id="control">
     <input type="button" value="white" onclick="document.getElementById('target').className='white'"/>
     <input type="button" value="black" onclick="document.getElementById('target').className='black'" />
-    <a href="http://localhost:8001/yahac/0622/write.php">쓰기</a>
+    <a href="http://localhost:8001/yahac/0623/write.php">쓰기</a>
   </div>
   <article>
     <form action="process.php" method="post">
